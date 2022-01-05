272597cc247f69d5b7e1cd1b2b20c9d7bbe288a7
Kevin Guertin <kevin.m.guertin@gmail.com>
2022-01-05 15:09:00 -0500

    use site metadata

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    siteurl: 'https://yourdomain.tld',
+    title: 'Frontend Masters intro to Gatsby',
+    description: 'Frontend masters intro to gatsby course project',
+    image:
+      'https://compote.slate.com/images/f3cd8572-1590-43fa-8cc1-d43792b0c20d.jpg',
+  },
+};
