233dadde81c775d864c4647653a6a80ce171042b
Kevin Guertin <kevin.m.guertin@gmail.com>
2022-01-11 21:57:26 -0500

    add images via page queries

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,26 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout.js';
 
-export default function AboutPage() {
+export const query = graphql`
+  query CocktailQuery {
+    file(name: { eq: "cocktail" }) {
+      childImageSharp {
+        gatsbyImageData(placeholder: DOMINANT_COLOR)
+      }
+    }
+  }
+`;
+
+export default function AboutPage({ data }) {
+  console.log(data);
   return (
     <Layout
       title="About This Site"
       description="More information about this site."
     >
+      <GatsbyImage image={getImage(data.file)} alt="A cocktail" />
       <h1>About This Site</h1>
       <Link to="/">Return to home</Link>
     </Layout>
